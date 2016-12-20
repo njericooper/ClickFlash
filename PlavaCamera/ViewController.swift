3f6c311e3107286eee18d746b1ff75709fa5d18f
njericooper <njericooper@gmail.com>
2016-12-20 15:12:07 -0600

    update

--- PlavaCamera/ViewController.swift
+++ PlavaCamera/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+@IBAction func openCameraButton(sender: AnyObject) {
+    weak var Camera: UIButton!
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+        var imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+        imagePicker.allowsEditing = false
+        self.presentViewController(imagePicker, animated:
+            true, completion: nil)
+        }
+    }
 
 }
 

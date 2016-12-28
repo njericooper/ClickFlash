0acc7d4f8f3723c736c8d58567d4b26538ec6305
njericooper <njericooper@gmail.com>
2016-12-27 18:33:39 -0600

    yo

--- Camera_New/ViewController.swift
+++ Camera_New/ViewController.swift
@@ -7,9 +7,51 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
-    @IBOutlet weak var imageView: UIImageView! 
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var newMedia: Bool?
+    
+    @IBAction func useCamera(sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.Camera){
+                let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.Camera
+            imagePicker.mediaTypes = [kUTTypeImage as NSString]
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated:true,
+                completion:nil)
+        newMedia = true
+        }
+    }
+    
+    @IBAction func useCameraRoll(sender: AnyObject){
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.Camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.Camera
+            imagePicker.mediaTypes = [kUTTypeImage as NSString]
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated:true,
+                                       completion:nil)
+            newMedia = true
+        }
+    }
+    
+    
+    
+    
+    @IBOutlet weak var useCameraRoll: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

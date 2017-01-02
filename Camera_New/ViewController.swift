f1580f373b951f1ba5d80bbc90dcdcca7e7e552f
njericooper <njericooper@gmail.com>
2017-01-02 17:14:31 -0600

    update

--- Camera_New/ViewController.swift
+++ Camera_New/ViewController.swift
@@ -9,61 +9,82 @@
 import UIKit
 import MobileCoreServices
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate,
-    UINavigationControllerDelegate {
-    
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
-   
     var newMedia: Bool?
     
-    @IBAction func useCamera(sender: AnyObject) {
-        
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerControllerSourceType.camera){
-                let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType =
-                UIImagePickerControllerSourceType.camera
-            imagePicker.mediaTypes = [(kUTTypeImage as NSString) as String]
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated:true,
-                completion:nil)
-        newMedia = true
-        }
-    }
-    
-    @IBAction func useCameraRoll(sender: AnyObject){
-        
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerControllerSourceType.camera){
+    @IBAction func useCamera(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
             let imagePicker = UIImagePickerController()
+            
             imagePicker.delegate = self
-            imagePicker.sourceType =
-                UIImagePickerControllerSourceType.camera
-            imagePicker.mediaTypes = [(kUTTypeImage as NSString) as String]
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.mediaTypes = [kUTTypeImage as String]
             imagePicker.allowsEditing = false
-            self.present(imagePicker, animated:true,
-                                       completion:nil)
+            self.present(imagePicker, animated:true, completion: nil)
             newMedia = true
         }
     }
     
     
+    @IBAction func useCameraRoll(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.savedPhotosAlbum) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated:true, completion: nil)
+            newMedia = false
+        }
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        if mediaType.isEqual(to: kUTTypeImage as String) {
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            
+            imageView.image = image
+            
+            if(newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(ViewController.image(image:didFinishSavingWithError:contextInfo:)), nil)
+                
+            } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+                
+            }
+        }
+    }
     
-    @IBOutlet weak var useCameraRoll: UIBarButtonItem!
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed", message: "Failed to save image", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true, completion:nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
